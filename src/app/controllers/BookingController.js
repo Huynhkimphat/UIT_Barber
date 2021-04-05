64ec768a6c51df9b6dc0555bb6a70282e96fef6a
Huynh Kim Phat <hkimphat2012@gmail.com>
2021-04-05 21:54:06 +0700

    Update New Version on Show Annd Delete Booking

--- src/app/controllers/BookingController.js
+++ src/app/controllers/BookingController.js
@@ -1,14 +1,38 @@
-const { showBooking, addBooking, destroy } = require("../../config/db");
+const { booking } = require("../../config/db");
 const { formatDate } = require("../../utils/formatDate");
 class BookingController {
     //* [GET]/
     show(req, res, next) {
+            (async() => {
+                if (process.env.status != 0) {
+                    let result = await booking.show();
+                    let temp = formatDate(result);
+                    res.render("booking/showBooking", {
+                        booking: temp,
+                        status: process.env.status,
+                        username: process.env.username,
+                    });
+                } else {
+                    res.redirect("/");
+                }
+            })();
+        }
+        // AddBooking(req, res, next) {
+        //     res.render("booking/addBooking");
+        // }
+        // Adding(req, res, next) {
+        //     (async() => {
+        //         let result = await addBooking(req.body.date);
+        //     })();
+        //     res.redirect("/booking");
+        // }
+    edit(req, res, next) {
         (async() => {
-            let result = await showBooking("DATLICH", 0);
-            let temp = formatDate(result.rows);
             if (process.env.status != 0) {
-                res.render("booking/showBooking", {
-                    booking: result.rows,
+                let result = await booking.show(req.params.id);
+                let temp = formatDate(result);
+                res.render("booking/updateBooking", {
+                    booking: temp,
                     status: process.env.status,
                     username: process.env.username,
                 });
@@ -17,18 +41,9 @@ class BookingController {
             }
         })();
     }
-    AddBooking(req, res, next) {
-        res.render("booking/addBooking");
-    }
-    Adding(req, res, next) {
-        (async() => {
-            let result = await addBooking(req.body.date);
-        })();
-        res.redirect("/booking");
-    }
     destroy(req, res, next) {
         (async() => {
-            let result = await destroy("DATLICH", req.params.id);
+            let result = await booking.destroy("DATLICH", req.params.id);
         })();
         res.redirect("/booking");
     }
--- src/routes/booking.js
+++ src/routes/booking.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const bookingController = require("../app/controllers/BookingController");
 const { route } = require("./authenticate");
 
-// router.get("/AddBooking", bookingController.AddBooking);
-// router.post("/AddBooking", bookingController.Adding);
 router.get("/", bookingController.show);
+router.get("/:id/edit", bookingController.edit);
 router.delete("/:id/", bookingController.destroy);
 // router.use('/search', siteController.search);
 
