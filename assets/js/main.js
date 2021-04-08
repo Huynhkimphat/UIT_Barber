f23809950f4d9d3f83ead0b3a8d1311bab586758
PhanTung-06 <19522495@gm.uit.edu.vn>
2021-04-08 16:48:06 +0700

    update full

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,7 @@
 var myCarousel = document.querySelector('#carouselExampleIndicators')
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: 1500,
-  wrap: true,
-  touch: true
+    interval: 2000,
+    wrap: true,
+    touch: true
 })
 
-
-$(document).ready(function(){
-    $('.single-item').slick({
-        
-    });
-  });
-      
-  
-$('.slider_product').slick({
-dots: true,
-infinite: true,
-speed: 2,
-slidesToShow: 4,
-slidesToScroll: 4,
-responsive: [
-    {
-    breakpoint: 1024,
-    settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-    }
-    },
-    {
-    breakpoint: 600,
-    settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-    }
-    },
-    {
-    breakpoint: 480,
-    settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-    }
-    }
-]
-});
