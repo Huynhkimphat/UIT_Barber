5bc054e454584ec51afff6bccf09818ad66399c3
PhanTung-06 <19522495@gm.uit.edu.vn>
2021-04-08 12:56:12 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var myCarousel = document.querySelector('#carouselExampleIndicators')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 1500,
+  wrap: true,
+  touch: true
+})
+
+
+$(document).ready(function(){
+    $('.single-item').slick({
+        
+    });
+  });
+      
+  
