743f80b56424d23f38f6ef37d7472cb629e108d6
Xuân Trường <62050602+XuanTruong2408@users.noreply.github.com>
2021-04-25 09:27:17 +0700

    adding

--- src/app/controllers/bookingController.js
+++ src/app/controllers/bookingController.js
@@ -34,6 +34,7 @@ class BookingController {
                 d.setDate(d.getDate() + 1);
                 dayString = d.toLocaleDateString("en-GB");
                 day.push([dayString]);
+                console.log(day);
                 res.render("booking/addBooking", {
                     // booking: temp,
                     day: day,
