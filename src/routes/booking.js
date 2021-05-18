d58e4aedbd714e11915cd534af081b2a36f0f046
HuynhKimPhat <kimphat.devcs@gmail.com>
2021-05-18 14:03:20 +0700

    Update JS in route

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -2,21 +2,16 @@ const express = require("express");
 const router = express.Router();
 const bookingController = require("../app/controllers/BookingController");
 
-
 router.get("/", bookingController.show);
-
 
 router.get("/:id/edit", bookingController.edit);
 router.delete("/:id/", bookingController.destroy);
 router.get("/:id/showDetail", bookingController.showDetail);
 
-
-
-// demo AJAX 
+// demo AJAX
 router.get("/add", bookingController.add);
-router.post("/addTimePeriod",bookingController.addTimePeriod);
-router.post("/addService",bookingController.addService);
+router.post("/addTimePeriod", bookingController.addTimePeriod);
+router.post("/addService", bookingController.addService);
 router.put("/", bookingController.adding);
-// router.use('/search', siteController.search);
 
 module.exports = router;
